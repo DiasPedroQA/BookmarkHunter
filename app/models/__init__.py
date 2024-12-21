466ccb08eabac1c76d2bf2fee7e17a8b7badd585
DiasPedroQA <diaspedro.dev@gmail.com>
2024-12-21 20:40:53 -0300

    teste-models-2
    Signed-off-by: DiasPedroQA <diaspedro.dev@gmail.com>

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,19 @@
-from .bookmark_model import Bookmark
-from .file_model import File
-from .folder_model import Folder
+# app/models/__init__.py
 
-__all__ = ["Bookmark", "File", "Folder"]
+"""
+Módulo de modelos.
+Este módulo contém as classes de modelos para o aplicativo.
+
+Classes:
+    TagProcessor: Classe para processar tags HTML.
+    Arquivo: Classe para representar um arquivo.
+    Pasta: Classe para representar uma pasta.
+
+Todos os modelos são importados neste módulo para facilitar o acesso.
+"""
+
+from .bookmark_model import TagProcessor
+from .file_model import Arquivo
+from .folder_model import Pasta
+
+__all__ = ["TagProcessor", "Arquivo", "Pasta"]
