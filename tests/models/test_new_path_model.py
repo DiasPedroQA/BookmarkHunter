8800cd4267988abee360425de58a00c222128e1b
DiasPedroQA <diaspedro.dev@gmail.com>
2025-01-20 03:04:34 -0300

    ...
    Signed-off-by: DiasPedroQA <diaspedro.dev@gmail.com>

--- tests/models/test_new_path_model.py
+++ tests/models/test_new_path_model.py
@@ -1,3 +1,5 @@
+# pylint: disable=C, R, E, W
+
 import unittest
 from app.models.new_path_model import AnalisadorCaminhoString, PathData
 
@@ -18,51 +20,42 @@ class TestAnalisadorCaminhoString(unittest.TestCase):
         """
         self.analisador = AnalisadorCaminhoString()
 
-    def test_caminho_windows(self):
-        """
-        Testa um caminho Windows com espaços nos nomes de diretórios.
-        """
-        caminho = "C:\\Program Files\\Aplicativos\\software.exe"
-        resultado = self.analisador.sanitizar_caminho(caminho)
-        esperado = PathData(
-            caminho_original=caminho,
-            caminho_sanitizado="C:\\ProgramFiles\\Aplicativos\\software.exe",
-            formato_valido=True,
-            extensao_arquivo=".exe",
-            eh_absoluto=True,
-            numero_diretorios=3,
-            nome_arquivo="software",
-            pasta_principal="Aplicativos",
-            eh_relativo=False,
-        )
-        self.assertEqual(resultado, esperado)
-
-    def test_caminho_inexistente(self):
-        """
-        Testa um caminho inexistente.
-        """
+    def test_caminho_invalido(self):
         caminho_teste = "/caminho/inexistente/"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
             caminho_original=caminho_teste,
-            caminho_sanitizado="/caminho/inexistente/",
+            caminho_sanitizado=caminho_teste,
             formato_valido=True,
-            eh_absoluto=False,
             extensao_arquivo=None,
+            eh_absoluto=False,
             numero_diretorios=2,
             nome_arquivo=None,
             pasta_principal="caminho",
             eh_relativo=False,
         )
-        self.assertEqual(resultado, esperado)
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
 
-    def test_caminho_absoluto_arquivo(self):
-        """
-        Testa um caminho absoluto para um arquivo HTML.
-        """
+    def test_caminho_absoluto_arquivo_existente(self):
         caminho_teste = "/home/pedro-pm-dias/Downloads/Chrome/favoritos_23_12_2024.html"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
             caminho_original=caminho_teste,
             caminho_sanitizado=caminho_teste,
             formato_valido=True,
@@ -73,15 +66,92 @@ class TestAnalisadorCaminhoString(unittest.TestCase):
             pasta_principal="Chrome",
             eh_relativo=False,
         )
-        self.assertEqual(resultado, esperado)
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
 
-    def test_caminho_relativo_html(self):
-        """
-        Testa um caminho relativo para um arquivo HTML.
-        """
+    def test_caminho_absoluto_arquivo_inexistente(self):
+        caminho_teste = "/home/pedro-pm-dias/Downloads/Chrome/favoritos.html"
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
+            caminho_original=caminho_teste,
+            caminho_sanitizado=caminho_teste,
+            formato_valido=True,
+            extensao_arquivo=".html",
+            eh_absoluto=True,
+            numero_diretorios=4,
+            nome_arquivo="favoritos",
+            pasta_principal="Chrome",
+            eh_relativo=False,
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
+
+    def test_caminho_absoluto_pasta_existente(self):
+        caminho_teste = "/home/pedro-pm-dias/Downloads/Chrome/"
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
+            caminho_original=caminho_teste,
+            caminho_sanitizado=caminho_teste,
+            formato_valido=True,
+            extensao_arquivo=None,
+            eh_absoluto=True,
+            numero_diretorios=4,
+            nome_arquivo=None,
+            pasta_principal="Downloads",
+            eh_relativo=False,
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
+
+    def test_caminho_relativo_arquivo_inexistente(self):
         caminho_teste = "../../Downloads/Chrome/favoritos.html"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
             caminho_original=caminho_teste,
             caminho_sanitizado=caminho_teste,
             formato_valido=True,
@@ -92,34 +162,60 @@ class TestAnalisadorCaminhoString(unittest.TestCase):
             pasta_principal="Chrome",
             eh_relativo=True,
         )
-        self.assertEqual(resultado, esperado)
-
-    def test_caminho_com_espacos(self):
-        """
-        Testa um caminho com espaços em nomes de diretórios.
-        """
-        caminho_teste = "/home/pedro-pm-dias/Meu Documento/arquivo.docx"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
-            caminho_original=caminho_teste,
-            caminho_sanitizado="/home/pedro-pm-dias/MeuDocumento/arquivo.docx",
-            formato_valido=True,
-            extensao_arquivo=".docx",
-            eh_absoluto=False,
-            numero_diretorios=3,
-            nome_arquivo="arquivo",
-            pasta_principal="MeuDocumento",
-            eh_relativo=False,
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
         )
-        self.assertEqual(resultado, esperado)
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
 
-    def test_caminho_com_til(self):
-        """
-        Testa um caminho com '~' para referenciar o diretório do usuário.
-        """
+    def test_caminho_relativo_arquivo_existente(self):
+        caminho_teste = "../../Downloads/Chrome/favoritos_23_12_2024.html"
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
+            caminho_original=caminho_teste,
+            caminho_sanitizado=caminho_teste,
+            formato_valido=True,
+            extensao_arquivo=".html",
+            eh_absoluto=False,
+            numero_diretorios=4,
+            nome_arquivo="favoritos_23_12_2024",
+            pasta_principal="Chrome",
+            eh_relativo=True,
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
+
+    def test_caminho_relativo_arquivo_inexistente(self):
         caminho_teste = "~/Downloads/Documentos/arquivo.txt"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
             caminho_original=caminho_teste,
             caminho_sanitizado="/Downloads/Documentos/arquivo.txt",
             formato_valido=True,
@@ -130,17 +226,30 @@ class TestAnalisadorCaminhoString(unittest.TestCase):
             pasta_principal="Documentos",
             eh_relativo=False,
         )
-        self.assertEqual(resultado, esperado)
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
 
-    def test_caminho_servidor(self):
-        """
-        Testa um caminho de rede compartilhada (servidor).
-        """
+    def test_caminho_absoluto_arquivo_inexistente(self):
         caminho_teste = "//server/share/arquivos/relatório.xlsx"
-        resultado = self.analisador.sanitizar_caminho(caminho=caminho_teste)
-        esperado = PathData(
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
             caminho_original=caminho_teste,
-            caminho_sanitizado="//server/share/arquivos/relatório.xlsx",
+            caminho_sanitizado="//server/share/arquivos/relatrio.xlsx",
             formato_valido=True,
             extensao_arquivo=".xlsx",
             eh_absoluto=False,
@@ -149,7 +258,87 @@ class TestAnalisadorCaminhoString(unittest.TestCase):
             pasta_principal="arquivos",
             eh_relativo=False,
         )
-        self.assertEqual(resultado, esperado)
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
+
+    def test_caminho_absoluto_arquivo_existente(self):
+        caminho_teste = "/home/pedro-pm-dias/Meu Documento/arquivo.docx"
+        objeto_obtido = (self.analisador.sanitizar_caminho(caminho=caminho_teste))
+        objeto_esperado = PathData(
+            caminho_original=caminho_teste,
+            caminho_sanitizado=caminho_teste,
+            formato_valido=False,
+            extensao_arquivo=".docx",
+            eh_absoluto=True,
+            numero_diretorios=3,
+            nome_arquivo="arquivo",
+            pasta_principal="Meu Documento",
+            eh_relativo=False
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_original, "/home/pedro-pm-dias/MeuDocumento/arquivo.docx"
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
+
+    def test_caminho_absoluto_windows_inexistente(self):
+        caminho_teste = "C:\Program Files\Aplicativos\software.exe"
+        objeto_obtido = self.analisador.sanitizar_caminho(caminho=caminho_teste)
+        objeto_esperado = PathData(
+            caminho_original="C:\Program Files\Aplicativos\software.exe",
+            caminho_sanitizado="C:\Program Files\Aplicativos\software.exe",
+            formato_valido=False,
+            extensao_arquivo=".exe",
+            eh_absoluto=True,
+            numero_diretorios=3,
+            nome_arquivo="software",
+            pasta_principal=None,
+            eh_relativo=False,
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_original, objeto_esperado.caminho_original
+        )
+        self.assertEqual(
+            objeto_obtido.caminho_sanitizado, objeto_esperado.caminho_sanitizado
+        )
+        self.assertEqual(objeto_obtido.formato_valido, objeto_esperado.formato_valido)
+        self.assertEqual(objeto_obtido.eh_absoluto, objeto_esperado.eh_absoluto)
+        self.assertEqual(
+            objeto_obtido.extensao_arquivo, objeto_esperado.extensao_arquivo
+        )
+        self.assertEqual(
+            objeto_obtido.numero_diretorios, objeto_esperado.numero_diretorios
+        )
+        self.assertEqual(objeto_obtido.nome_arquivo, objeto_esperado.nome_arquivo)
+        self.assertEqual(objeto_obtido.pasta_principal, objeto_esperado.pasta_principal)
+        self.assertEqual(objeto_obtido.eh_relativo, objeto_esperado.eh_relativo)
 
 
 if __name__ == "__main__":
